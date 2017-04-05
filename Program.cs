a9fd7af658e21008dafc7e57f91fd58af5e836cf
Bill Peterson <william.o.peterson@gmail.com>
2017-04-05 15:07:54 -0700

    UI Changes Crazy Eights v1.0.1

--- Program.cs
+++ Program.cs
@@ -18,11 +18,11 @@ namespace ConsoleApplication
             Console.WriteLine("Enter first player's name.");
             inputline  = Console.ReadLine();
             Player player1 = new Player(inputline);
-            System.Console.WriteLine(player1.name);
-            Console.WriteLine("Enter second player's name.");
+
+            Console.WriteLine("\nEnter second player's name.");
             inputline  = Console.ReadLine();
             Player player2 = new Player(inputline);
-            System.Console.WriteLine(player2.name);
+
             // add players to list, create turn counter
             // give players 7 cards
             for (int i = 1; i <= 7; i++){
@@ -45,16 +45,20 @@ namespace ConsoleApplication
                     turn = 0;
                 }
 
+                Console.ForegroundColor = ConsoleColor.Green;
+                System.Console.WriteLine("\n************************************" + players[turn].name.ToUpper() +"'s TURN************************************");
+                Console.ForegroundColor = ConsoleColor.White;
+
 
 
                 //show hand
-                System.Console.WriteLine(players[turn].name + "'s hand: " + players[turn].ShowHand());
+                System.Console.WriteLine("\n" + players[turn].name + "'s hand: " + players[turn].ShowHand());
 
                 //show the discard pile
-                System.Console.WriteLine("The top card on the discard pile is a(n) " + myDeck.ShowDiscard().ToString());
+                System.Console.WriteLine("\nThe top card on the discard pile is a(n) " + myDeck.ShowDiscard().ToString());
 
                 //prompt which card to play
-                System.Console.WriteLine("Enter a command - Draw or Quit - or enter a card number between 1 and " + players[turn].hand.Count);
+                System.Console.WriteLine("\nEnter a command - Draw or Quit - or enter a card number between 1 and " + players[turn].hand.Count);
 
                 inputline = Console.ReadLine().ToLower();
                 //check if input is an integer and within the hand range
@@ -70,59 +74,73 @@ namespace ConsoleApplication
                             break;
                         }
                         else {
-                            System.Console.WriteLine("You can only pass if the draw pile is empty.");
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            System.Console.WriteLine("\nYou can only pass if the draw pile is empty.");
+                            Console.ForegroundColor = ConsoleColor.White;
                             turn--;
                             break;
                         }                        
                     case "quit":
                         gameOn = false;
-                        System.Console.WriteLine(players[turn].name + " is giving up like a big whiny baby.");
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        System.Console.WriteLine("\n" + players[turn].name + " is giving up like a big whiny baby.");
+                        Console.ForegroundColor = ConsoleColor.White;
                         break;
                     default:
                         if (int.TryParse(inputline, out cardNum) && cardNum <= players[turn].hand.Count && cardNum > 0){
                             cardNum--;
                             if(players[turn].hand[cardNum].val == 8){
-                                System.Console.WriteLine("Please enter the suit for your Crazy Eight:");
+                                System.Console.WriteLine("\n" +  players[turn].name + ", please enter the suit (- Hearts - Clubs - Diamonds - Spades -) for your Crazy Eight:");
                                 inputline = Console.ReadLine();
                                 //Change the suit of the Eight
                                 if(myDeck.suits.Contains(inputline)){
                                     players[turn].hand[cardNum].suit = inputline;
                                     myDeck.discards.Add(players[turn].Discard(cardNum));
-                                    // System.Console.WriteLine("That's totally a suit.");
+                                    // System.Console.WriteLine("\nThat's totally a suit.");
                                     if (players[turn].hand.Count <= 0){
-                                        System.Console.WriteLine("Congratulations, " + players[turn].name + ", you have won!");
+                                        Console.ForegroundColor = ConsoleColor.Green;
+                                        System.Console.WriteLine("\n*******************CONGRATULATIONS, " + players[turn].name + ", YOU HAVE WON!*********************");
+                                        Console.ForegroundColor = ConsoleColor.White;
                                         gameOn = false;
                                         return;
                                     }
                                 }
                                 else {
-                                    System.Console.WriteLine("Please enter a valid suit: - Hearts - Clubs - Diamonds - Spades -");
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    System.Console.WriteLine("\n" + players[turn].name + ", please enter a valid suit: - Hearts - Clubs - Diamonds - Spades -");
+                                    Console.ForegroundColor = ConsoleColor.White;
                                     turn--;
                                 }
 
-                                // System.Console.WriteLine("You played a card." + players[turn].hand[cardNum-1].ToString());
+                                // System.Console.WriteLine("\nYou played a card." + players[turn].hand[cardNum-1].ToString());
                             }
                             else if(players[turn].hand[cardNum].val == myDeck.ShowDiscard().val || players[turn].hand[cardNum].suit == myDeck.ShowDiscard().suit){
                                 myDeck.discards.Add(players[turn].Discard(cardNum));
                                 if (players[turn].hand.Count <= 0){
-                                    System.Console.WriteLine("Congratulations, " + players[turn].name + ", you have won!");
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                    System.Console.WriteLine("\n*******************CONGRATULATIONS, " + players[turn].name + ", YOU HAVE WON!*********************");
+                                    Console.ForegroundColor = ConsoleColor.White;
                                     gameOn = false;
                                     return;
                                   }
                             }
                             else {
-                                System.Console.WriteLine("Not a valid card to play, please pick a different card or draw.");
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                System.Console.WriteLine("\n" + players[turn].name + ", this is not a valid card to play, please pick a different card or draw.");
+                                Console.ForegroundColor = ConsoleColor.White;
                                 turn--;
                             }
                         }
                         else {
-                            System.Console.WriteLine("Not a valid card number, please choose a card between 1 and " + players[turn].hand.Count + " or enter a command.");
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            System.Console.WriteLine("\n" + players[turn].name + ", this is not a valid card number, please choose a card between 1 and " + players[turn].hand.Count + " or enter a command.");
+                            Console.ForegroundColor = ConsoleColor.White;
                             turn--;
                         }
                         break;
                 }
                 turn++;
-
+                
 
                 //check if the card can be played
 
@@ -131,7 +149,7 @@ namespace ConsoleApplication
 
             // myDeck.ShowDiscard();
 
-            // System.Console.WriteLine(player1.name + "'s hand: " + player1.ShowHand());
+            // System.Console.WriteLine(p\nlayer1.name + "'s hand: " + player1.ShowHand());
             // System.Console.WriteLine(player2.name + "'s hand: " + player2.ShowHand());
 
             // myDeck.discards.Add(player1.Discard(1));
